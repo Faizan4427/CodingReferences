4e74c4e13131540c7ef95af07e70365696a6f3bb
Faizan4427 <faizan4427@gmail.com>
2025-04-17 15:37:03 +0530

    added UnityUIToolkit_BindingListViewAtRuntime.cs

---
+++ UnityUIToolkit_BindingListViewAtRuntime.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace CodingReferences
+{
+    public class UnityUIToolkit_BindingListViewAtRuntime : MonoBehaviour
+    {
+        [SerializeField] private UIDocument _uiDocument;
+        [SerializeField] private List<int> _list;
+        
+        private void Start()
+        {
+            var root = new VisualElement();
+            var listView = new ListView {
+                dataSource = this,
+                itemsSource = _list,
+                makeItem = () => new Label(),
+                bindItem = (e, i) => {
+                    var label = (Label)e;
+                    label.text = _list[i].ToString();
+                },
+                unbindItem = (e, i) => {
+                },
+                destroyItem = e => {
+                }
+            };
+            
+            root.Add(listView);
+            _uiDocument.rootVisualElement.Add(root);
+        }
+    }
+}
